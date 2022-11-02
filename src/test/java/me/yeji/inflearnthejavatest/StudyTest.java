b42666b4f0b5086dc584cc7b6921ef6067648b45
jeong-yeji <jeongyeji.mail@gmail.com>
2022-11-03 08:38:14 +0900

    JUnit5: 11. Test Order

--- src/test/java/me/yeji/inflearnthejavatest/StudyTest.java
+++ src/test/java/me/yeji/inflearnthejavatest/StudyTest.java
@@ -5,12 +5,16 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.TestMethodOrder;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @TestInstance(Lifecycle.PER_CLASS)
+@TestMethodOrder(OrderAnnotation.class)
 class StudyTest {
 
     int value = 1;
@@ -18,6 +22,7 @@ class StudyTest {
     @Test
     @DisplayName("스터디 만들기 fast")
     @FastTest
+    @Order(2)
     void create_new_study() {
         System.out.println(this);
         System.out.println(value++);
@@ -25,9 +30,11 @@ class StudyTest {
         assertNotNull(study);
     }
 
+
     @Test
     @DisplayName("스터디 만들기 slow")
     @SlowTest
+    @Order(1)
     void create_new_study_again() {
         System.out.println(this);
         System.out.println("Create Again" + value++);
