0210202d7f11ea96f46523dbf258d1ae63976b73
jeong-yeji <jeongyeji.mail@gmail.com>
2022-11-03 07:56:52 +0900

    JUnit5: 08. RepeatedTest and ParameterizedTest

--- src/test/java/me/yeji/inflearnthejavatest/StudyTest.java
+++ src/test/java/me/yeji/inflearnthejavatest/StudyTest.java
@@ -5,7 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
@@ -24,5 +28,19 @@ class StudyTest {
     @SlowTest
     void create_new_study_again() {
         System.out.println("Create Again");
+    }
+
+    @DisplayName("반복 스터디")
+    @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition}/{totalRepetitions}")
+    void repeat_test(RepetitionInfo repetitionInfo) {
+        System.out.println(
+                "test " + repetitionInfo.getCurrentRepetition() + "/" + repetitionInfo.getTotalRepetitions());
+    }
+
+    @DisplayName("다른 매개변수")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(strings = {"날씨가", "많이", "춥다" })
+    void parameterizedTest(String message) {
+        System.out.println(message);
     }
 }
