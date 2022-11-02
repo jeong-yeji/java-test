1a9f399bc48f6b409de30d1be189c0133970ebd9
jeong-yeji <jeongyeji.mail@gmail.com>
2022-11-03 00:23:27 +0900

    JUnit5: 04. Assertion

---
+++ src/main/java/me/yeji/inflearnthejavatest/StudyStatus.java
@@ -0,0 +1,5 @@
+package me.yeji.inflearnthejavatest;
+
+public enum StudyStatus {
+    DRAFT, STARTED, ENDED
+}
--- src/test/java/me/yeji/inflearnthejavatest/StudyTest.java
+++ src/test/java/me/yeji/inflearnthejavatest/StudyTest.java
@@ -1,7 +1,13 @@
 package me.yeji.inflearnthejavatest;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.time.Duration;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -17,14 +23,25 @@ class StudyTest {
     @Test
     @DisplayName("스터디 만들기 \uD83D\uDE31")
     void create_new_study() {
-        Study study = new Study();
-        assertNotNull(study);
-        System.out.println("create");
+        Study study = new Study(-10);
+        assertAll(
+                () -> assertNotNull(study),
+                () -> assertEquals(StudyStatus.DRAFT, study.getStatus(), "스터디를 처음 만들면 상태값이 DRAFT여야 한다."),
+                () -> assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야 한다.")
+        );
     }
 
     @Test
     void create_new_study_again() {
-        System.out.println("create1");
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> new Study(-10));
+        assertEquals("limit은 0보다 커야한다", exception.getMessage());
+
+        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
+//        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
+            new Study(10);
+            Thread.sleep(300);
+        });
     }
 
     @BeforeAll
