0518ac53c50d49cc927dd1bc6fce8fbee1da302e
jeong-yeji <jeongyeji.mail@gmail.com>
2022-11-03 08:46:42 +0900

    JUnit5: 12. JUnit properties

--- src/test/java/me/yeji/inflearnthejavatest/StudyTest.java
+++ src/test/java/me/yeji/inflearnthejavatest/StudyTest.java
@@ -2,18 +2,13 @@ package me.yeji.inflearnthejavatest;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.TestMethodOrder;
 
-@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-@TestInstance(Lifecycle.PER_CLASS)
 @TestMethodOrder(OrderAnnotation.class)
 class StudyTest {
 
@@ -32,9 +27,9 @@ class StudyTest {
 
 
     @Test
-    @DisplayName("스터디 만들기 slow")
     @SlowTest
     @Order(1)
+    @Disabled
     void create_new_study_again() {
         System.out.println(this);
         System.out.println("Create Again" + value++);
