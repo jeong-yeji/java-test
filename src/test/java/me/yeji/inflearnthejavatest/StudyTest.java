31ebd1a7dc9a5baf698138ec6b54c9d396cd6e62
jeong-yeji <jeongyeji.mail@gmail.com>
2022-11-03 01:01:36 +0900

    JUnit5: 06. Tagging and Filtering

--- src/test/java/me/yeji/inflearnthejavatest/StudyTest.java
+++ src/test/java/me/yeji/inflearnthejavatest/StudyTest.java
@@ -1,84 +1,29 @@
 package me.yeji.inflearnthejavatest;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
-import java.time.Duration;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledOnOs;
-import org.junit.jupiter.api.condition.EnabledIf;
-import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
-import org.junit.jupiter.api.condition.EnabledOnJre;
-import org.junit.jupiter.api.condition.EnabledOnOs;
-import org.junit.jupiter.api.condition.JRE;
-import org.junit.jupiter.api.condition.OS;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 
     @Test
-    @DisplayName("스터디 만들기 \uD83D\uDE31")
-    @EnabledOnOs(OS.WINDOWS)
-    @EnabledOnJre({JRE.JAVA_8, JRE.JAVA_9, JRE.JAVA_10, JRE.JAVA_11})
+    @DisplayName("스터디 만들기 fast")
+    @Tag("fast")
     void create_new_study() {
-        String test_env = System.getenv("TEST_ENV");
-        System.out.println(test_env);
-        assumeTrue("LOCAL".equalsIgnoreCase(test_env));
 
-        Study study = new Study(-10);
-        assertAll(
-                () -> assertNotNull(study),
-                () -> assertEquals(StudyStatus.DRAFT, study.getStatus(), "스터디를 처음 만들면 상태값이 DRAFT여야 한다."),
-                () -> assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야 한다.")
-        );
+        Study study = new Study(10);
+        assertNotNull(study);
     }
 
     @Test
-    @DisabledOnOs({OS.MAC, OS.LINUX})
-    @EnabledOnJre(JRE.OTHER)
-    @EnabledIfEnvironmentVariable(named = "TEST_ENV", matches = "yeji")
-    @EnabledIf()
+    @DisplayName("스터디 만들기 slow")
+    @Tag("slow")
     void create_new_study_again() {
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
-                () -> new Study(-10));
-        assertEquals("limit은 0보다 커야한다", exception.getMessage());
-
-        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
-//        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
-            new Study(10);
-            Thread.sleep(300);
-        });
-    }
-
-    @BeforeAll
-    static void beforeAll() {
-        System.out.println("before all");
-    }
-
-    @AfterAll
-    static void afterAll() {
-        System.out.println("after all");
-    }
-
-    @BeforeEach
-    void beforeEach() {
-        System.out.println("before each");
-    }
-
-    @AfterEach
-    void afterEach() {
-        System.out.println("after each");
+        System.out.println("Create Again");
     }
 }
